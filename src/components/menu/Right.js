94328fb2985baf296d9ddb32cd29424cd677388c
unknown <jorammanoah1@gmail.com>
2021-05-05 23:52:19 -0700

    Added navbar

---
+++ src/components/menu/Right.js
@@ -0,0 +1,12 @@
+import { Menu } from 'antd'
+import React from 'react'
+
+export default function Right() {
+    return (
+        <Menu>
+            <Menu.Item>
+                Contact us
+            </Menu.Item>
+        </Menu>
+    )
+}
