5a38a4d44ca4522ff8444608b92fb3114141ac32
unknown <jorammanoah1@gmail.com>
2021-05-06 16:18:55 -0700

    Removed background color

--- src/components/menu/NavBar.js
+++ src/components/menu/NavBar.js
@@ -12,9 +12,9 @@ export default function NavBar() {
     setVisible(false);
   }
   return (
-    <nav className="menu" style={{backgroundColor:"#1890ff", position:"static", top:"0", width:"100%", zIndex:"1"}}>
+    <nav className="menu">
       <div className="menu__logo">
-        <a href="/" style={{color:"white"}}>LOGO</a>
+        <a href="/">Logo</a>
       </div>
       <div className="menu__container">
         <div className="menu_rigth" style={{ marginTop: ".7rem" }}>
