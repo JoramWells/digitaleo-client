5f80620c6492d1ae62046ae537f2438320e4616d
unknown <jorammanoah1@gmail.com>
2021-05-06 01:02:10 -0700

    Modified css

--- src/components/menu/NavBar.js
+++ src/components/menu/NavBar.js
@@ -1,44 +1,42 @@
-import React, { useState } from 'react'
-import Right from './Right'
-import {Button, Drawer} from 'antd'
-import {AlignRightOutlined} from '@ant-design/icons'
+import React, { useState } from "react";
+import Right from "./Right";
+import { Button, Drawer } from "antd";
+import { AlignRightOutlined } from "@ant-design/icons";
 
 export default function NavBar() {
-    const [visible, setVisible] = useState(false)
-    function showDrawer(){
-        setVisible(true)
-    }
-    function hideDrawer(){
-        setVisible(false)
-    }
-    return (
-        <nav className="menu">
-            <div className="menu__logo">
-                <a href="/">Logo</a>
-            </div>
-            <div className="menu__container" >
-                <div className="menu_rigth" style={{marginTop:".7rem"}}>
-                    <Right mode="horizontal"/>
-                </div>
-            </div>
-            <Button
-            className="menu__mobile-button"
-            type="primary"
-            onClick={showDrawer}
-            >
-                <AlignRightOutlined/>
-            </Button>
-            <Drawer
-          title="Basic Drawer"
-          placement="right"
-          className="menu_drawer"
-          closable={false}
-          onClose={hideDrawer}
-          visible={visible}
-        >
-          <Right mode="inline" />
-        </Drawer>
-            
-        </nav>
-    )
+  const [visible, setVisible] = useState(false);
+  function showDrawer() {
+    setVisible(true);
+  }
+  function hideDrawer() {
+    setVisible(false);
+  }
+  return (
+    <nav className="menu">
+      <div className="menu__logo">
+        <a href="/">Logo</a>
+      </div>
+      <div className="menu__container">
+        <div className="menu_rigth" style={{ marginTop: ".7rem" }}>
+          <Right mode="horizontal" />
+        </div>
+      </div>
+
+      <AlignRightOutlined
+        className="menu__mobile-button"
+        style={{ fontSize: "1.4rem" }}
+        onClick={showDrawer}
+      />
+      <Drawer
+        title="Basic Drawer"
+        placement="right"
+        className="menu_drawer"
+        closable={false}
+        onClose={hideDrawer}
+        visible={visible}
+      >
+        <Right mode="inline" />
+      </Drawer>
+    </nav>
+  );
 }
